b3f8419def5c9158ad07cb9de8bd466cc66044ea
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-05-18 16:34:48 +0530

    Add files via upload

--- Numerical Analysis/Euler.c
+++ Numerical Analysis/Euler.c
@@ -1,3 +1,7 @@
+/* 
+Euler Method
+Roll no.: MTUG/118/19
+*/
 #include<stdio.h>
 #include<math.h>
 float f(float x, float y)
@@ -7,11 +11,17 @@ float f(float x, float y)
 
 void main()
 {
-    float x0=0,xn=0.4,h=0.2,y;
+    float x0=0,xn=0.4,h=0.2,y,x;
     int n=(xn-x0)/h,i;
     for(i=0;i<n;i++)
     {
         y=y+h*f(xn,y);
+        x=x0+(i+1)*h;
+        printf("\n y(%0.1f)=%0.4f",x,y);
     }
-    printf("Result=> %f",y);
+    getchar();
 }
+/* Output
+ y(0.2)=-0.0796
+ y(0.4)=-0.1784
+*/
--- Numerical Analysis/RK4.c
+++ Numerical Analysis/RK4.c
@@ -1,4 +1,9 @@
+/* 
+Runge Kutta 4 Method
+Roll no.: MTUG/118/19
+*/
 #include<stdio.h>
+#define n 5
 float f(float x,float y)
 {
     return (x*x)+(y*y);
@@ -6,12 +11,22 @@ float f(float x,float y)
 
 void main()
 {
-    float x0=1,y0=0,h=0.2,k1,k2,k3,k4,y;
-    k1 =f(x0,y0);
-    k2 = h*f(x0+(h/2),y0+(k1/2));
-    k3 = h*f(x0+(h/2),y0+(k2/2));
-    k4 = h*f(x0 + h, y0 + k3);
-    y  = y0 + (k1 + 2*k2 + 2*k3 + k4)/6;
+    float x0=1,y0=0,xn=1.2,h,k1,k2,k3,k4,y;
+    h=(xn-x0)/n;
+    for(int i=0;i<n;i++) 
+    {
+        k1 = h*f(x0,y0);
+        k2 = h*f(x0+(h/2),y0+(k1/2));
+        k3 = h*f(x0+(h/2),y0+(k2/2));
+        k4 = h*f(x0 + h, y0 + k3);
+    y = y0 + ((k1 + 2*k2 + 2*k3 + k4)/6);
+    x0+=h;
+    y0=y;
+    }
     printf("\n Value at required point=>%f",y); 
     getchar();
 }
+
+/* Output
+ Value at required point=>0.246313
+*/
