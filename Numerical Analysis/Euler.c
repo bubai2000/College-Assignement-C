ef995b1f5111df778d9c6fe359b6abb7088a3847
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-05-19 15:14:19 +0530

    Add files via upload

--- Numerical Analysis/Euler.c
+++ Numerical Analysis/Euler.c
@@ -11,17 +11,20 @@ float f(float x, float y)
 
 void main()
 {
-    float x0=0,xn=0.4,h=0.2,y,x;
-    int n=(xn-x0)/h,i;
+    float x0=0,xn=0.4,h=0.2,y0=1.0;
+    int n,i;
+    n=(int)((xn-x0)/h);
     for(i=0;i<n;i++)
     {
-        y=y+h*f(xn,y);
-        x=x0+(i+1)*h;
-        printf("\n y(%0.1f)=%0.4f",x,y);
+        y0+=h*f(x0,y0);
+        x0+=h;
+        printf("\n y(%0.1f)=%0.4f",x0,y0);
     }
     getchar();
 }
 /* Output
- y(0.2)=-0.0796
- y(0.4)=-0.1784
+
+ y(0.2)=1.0000
+ y(0.4)=1.0158
+
 */
