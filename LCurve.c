2fdc8874311a961085e3d1414999b29dec3a7c91
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-05-20 16:32:26 +0530

    Add files via upload

---
+++ LCurve.c
@@ -0,0 +1,41 @@
+/* Linear curve Fitting
+MTUG/118/19
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float x,y,sx=0,sy=0,sxy=0,sx2=0,a,b;
+    int n;
+    printf("Enter no of points=>");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter x %d and y %d =>", i+1,i+1);
+        scanf("%f%f",&x,&y);
+        sx+=x;
+        sy+=y;
+        sxy+=(x*y);
+        sx2+=(x*x);
+    }
+    b=((sx*sy)-(n*sxy))/((sx*sx)-(n*sx2));
+    a=((sy-(b*sx))/n);
+    printf("\n\nThe line is Y=%f +%f X",a,b);
+    getchar();
+}
+/*Output
+Enter no of points=>9
+Enter x 1 and y 1 =>0 119
+Enter x 2 and y 2 =>1 141
+Enter x 3 and y 3 =>2 144
+Enter x 4 and y 4 =>3 149
+Enter x 5 and y 5 =>4 150
+Enter x 6 and y 6 =>5 158
+Enter x 7 and y 7 =>6 161
+Enter x 8 and y 8 =>7 166
+Enter x 9 and y 9 =>8 170
+
+
+The line is Y=129.422226 +5.366667 X
+*/
+     
