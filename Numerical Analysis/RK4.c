f1b39722c09dffe663a1ba6d30a3dba0e7f4fb78
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-05-22 15:41:42 +0530

    Update RK4

--- Numerical Analysis/RK4.c
+++ Numerical Analysis/RK4.c
@@ -3,7 +3,6 @@ Runge Kutta 4 Method
 Roll no.: MTUG/118/19
 */
 #include<stdio.h>
-#define n 5
 float f(float x,float y)
 {
     return (x*x)+(y*y);
@@ -11,20 +10,19 @@ float f(float x,float y)
 
 void main()
 {
-    float x0=1,y0=0,xn=1.2,h,k1,k2,k3,k4,y;
-    h=(xn-x0)/n;
+    float x0=1,y0=0,xn=1.2,h=0.1,k1,k2,k3,k4;
+    int n=(int)((xn-x0)/h);
+    printf("%d",n);
     for(int i=0;i<n;i++) 
     {
         k1 = h*f(x0,y0);
         k2 = h*f(x0+(h/2),y0+(k1/2));
         k3 = h*f(x0+(h/2),y0+(k2/2));
         k4 = h*f(x0 + h, y0 + k3);
-    y = y0 + ((k1 + 2*k2 + 2*k3 + k4)/6);
-    x0+=h;
-    y0=y;
+        y0 += ((k1 + 2*k2 + 2*k3 + k4)/6);
+        x0+=h;
     }
-    printf("\n Value at required point=>%f",y); 
-    getchar();
+    printf("\n Value at required point=>%f",y0); 
 }
 
 /* Output
