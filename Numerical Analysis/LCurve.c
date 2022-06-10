b8b413f34b866524d1035ace48529a39c57ce22b
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-06-10 09:31:06 +0530

    Update

--- Numerical Analysis/LCurve.c
+++ Numerical Analysis/LCurve.c
@@ -24,10 +24,10 @@ void main()
 }
 /*Output
 Enter no of points=>9
-Enter x 1 and y 1 =>0 19
+Enter x 1 and y 1 =>0 119
 Enter x 2 and y 2 =>1 141
 Enter x 3 and y 3 =>2 144
-Enter x 4 and y 4 =>3 149 
+Enter x 4 and y 4 =>3 149
 Enter x 5 and y 5 =>4 150
 Enter x 6 and y 6 =>5 158
 Enter x 7 and y 7 =>6 161
@@ -35,6 +35,6 @@ Enter x 8 and y 8 =>7 166
 Enter x 9 and y 9 =>8 170
 
 
-The line is Y=91.644440 +12.033334 X
+The line is Y=129.422226 +5.366667 X 
 */
      
--- Numerical Analysis/PCurve.c
+++ Numerical Analysis/PCurve.c
@@ -4,7 +4,7 @@ MTUG/118/19
 #include<stdio.h>
 void main()
 {
-    float x,y,sx=0,sx2=0,sx3=0,sx4=0,sy=0,sxy=0,sx2y=0,a,b,c;
+    float x,y,sx=0,sx2=0,sx3=0,sx4=0,sy=0,sxy=0,sx2y=0,a,b,c,w;
     int n;
     printf("Enter no of points=>");
     scanf("%d",&n);
@@ -20,23 +20,24 @@ void main()
         sxy+=(x*y);
         sx2y+=(x*x*y);
     }
-    a=(sy*((sx2*sx4)-(sx3*sx3))+sxy*((sx2*sx3)-(sx*sx4))+sx2y*((sx*sx3)-(sx2*sx2)))/(n*((sx2*sx4)-(sx3*sx3))+sx*((sx3*sx2)-(sx*sx4))+sx2*((sx*sx3)-(sx2*sx2)));
-    printf("\n a=%f",a);
+    w=n*((sx2*sx4)-(sx3*sx3))-sx*((sx*sx4)-(sx2*sx3))+sx2*((sx*sx3)-(sx2*sx2));
+    a=(sy*((sx2*sx4)-(sx3*sx3))+sxy*((sx3*sx2)-(sx*sx4))+sx2y*((sx*sx3)-(sx2*sx2)))/w;
+    b=(sy*((sx2*sx3)-(sx*sx4))+sxy*((n*sx4)-(sx2*sx2))+sx2y*((sx*sx2)-(n*sx3)))/w;
+    c=(sy*((sx*sx3)-(sx2*sx2))+sxy*((sx*sx2)-(n*sx3))+sx2y*((n*sx2)-(sx*sx)))/w;
+    printf("\nThe quadratic equation is=> Y=(%.5f)+(%.5f) X+(%.5f) X^2",a,b,c);
     getchar();
 }
 /*Output
-Enter no of points=>9
-Enter x 1 and y 1 =>0 119
-Enter x 2 and y 2 =>1 141
-Enter x 3 and y 3 =>2 144
-Enter x 4 and y 4 =>3 149
-Enter x 5 and y 5 =>4 150
-Enter x 6 and y 6 =>5 158
-Enter x 7 and y 7 =>6 161
-Enter x 8 and y 8 =>7 166
-Enter x 9 and y 9 =>8 170
+Enter no of points=>8
+Enter x 1 and y 1 =>1 -5
+Enter x 2 and y 2 =>2 -12.4
+Enter x 3 and y 3 =>3 -15.7
+Enter x 4 and y 4 =>4 -15.1
+Enter x 5 and y 5 =>5 -10.5
+Enter x 6 and y 6 =>6 -1.9
+Enter x 7 and y 7 =>7 10.7
+Enter x 8 and y 8 =>8 24.4
 
-
-The line is Y=129.422226 +5.366667 X
+The quadratic equation is=> Y=(5.25177)+(-12.49701) X+(1.87440) X^2
 */
      
